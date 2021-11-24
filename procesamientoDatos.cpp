a2796cec57b58b5f653760f71444b985751e1f63
Andres de la Roca <andresdelaroca11@gmail.com>
2021-11-24 12:05:50 -0600

    Update a programa sensorsonido y procesamiento de datos
    Co-authored-by: MarcoJurado20308 <MarcoJurado20308@users.noreply.github.com>
    Co-authored-by: José Auyón <auyjos@users.noreply.github.com>

--- procesamientoDatos.cpp
+++ procesamientoDatos.cpp
@@ -5,7 +5,7 @@
  * integrantes: 
  *      Marco Jurado 20308
  *      Andrés de la Roca 20332
- *      José Auyón
+ *      José Auyón 20157
  * 
  * Procesamiento de datos del sensor de valor
  * de sonido hacia decibeles.
@@ -14,3 +14,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <json/value.h>
+#include <fstream>
+
+int main(int argc, char* argv[]) 
+{
+    
+    
+    float led1V= 1.7
+    float led2V= 1.8
+    float led3V= 1.6
+
+    float led1_amp= 0.3 
+    float led2_amp=0.3
+    float led3_amp= 0.3
+
+    float voltaje= led1+led2+led3;
+    printf("El consumo en voltios de las leds es %f", voltaje);
+    
+    float amperaje= led1_amp+led2_amp+ led3_amp
+    printf("El amperaje de las leds es %f", amperaje);
+
+    poder= voltaje*amperaje;
+    printf("El poder en watts es %.2f watts\n", poder)
+
+    int diario= (60*60*24);
+    int semanal= (7*24*60*60);
+    int mensual= (31*24*60*60);
+    int anual=  (365*24*60*60);
+
+    float kilo_Watts= 1000;
+
+    int consumo_minutoW= poder*60;
+    int consumo_diarioW= poder * diario;
+    int consumo_ semanalW=poder*semanal;
+    int consumo_mensualW= poder* mensual;
+    int consumo_anualW= poder* anual;
+
+    float consumo_minutoKhw= consumo_minutoW/kilo_Watts;
+    float consumo_diarioKhw= consumo_diarioW/kilo_Watts;
+    float consumo_semanalKhw= consumo_semanalW/kilo_Watts
+    float consumo_mensualKhw= consumo_mensualW/kilo_Watts;
+    float consumo_anualKhw = consumo_anualW/kilo_Watts;
+
+
+    print("El consumo por minuto es de %d watts", consumo_minutoW + "o %f KWhw ", consumo_minutoKhw );
+    print("El consumo diario es de %d watts", consumo_diarioW + "o %f KWhw ", consumo_diarioKhw );
+    print("El consumo diario es de %d watts", consumo_diarioW + "o %f KWhw ", consumo_diarioKhw )
+
+    return 0;
+}
--- sensorsonido.py
+++ sensorsonido.py
@@ -6,48 +6,119 @@
 # .JSON.
 #-----------------------------------------
 # Universidad del Valle de Guatemala
-# CC3056-10 Programación de microprocesadores
+# CC3056-10 Programacion de microprocesadores
 # Proyecto Microprocesadores grupo 6
 # integrantes: 
 # Marco Jurado - 20308
-# Jose Auyon - 
+# Jose Auyon - 201579
 # Andres de la Roca - 20332
 #-----------------------------------------
 
 import time
 import json
 import random
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
+import csv
 
 #Setup del GPIO de la Raspy
-canalAnalogo = No. de pin de la raspy en el que se conecta el output del sensor
+
+canalLed1 = 17
+canalLed2 = 27
+canalLed3 = 22
+canalSensor = 23
 #canalDigital =
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(canalAnalogo, GPIO.IN)
+GPIO.setup(canalSensor, GPIO.IN) #Puerto que controla el sensor
+GPIO.setup(canalLed1, GPIO.OUT)
+GPIO.setup(canalLed2, GPIO.OUT)
+GPIO.setup(canalLed3, GPIO.OUT)
 
 
-dictMedicion = {
-    "Datos_Sensor":[]
-}
+dictMedicion = []
 
-def write_json(datos_nuevos, filename="data.json"):
-    with open(filename, 'w') as fp:
-        json.dump(datos_nuevos, fp) 
+def write_csv(datos_nuevos, filename="data.csv"):
     
-
-try:
-    while True:
-        sonido = GPIO.input(canalAnalogo)
-
-        """dictMedicion["Datos_Sensor"].append({
-            "Unidad de Medida Sonido": str(sonido) + " Placeholder ",
-        })"""
+    with open(filename, 'w') as fp:
+        writer = csv.writer(fp)
+        for value in dictMedicion:
+            writer.writerow(value)
 
 
-        #print(dictMedicion)
-        print("Valor de sonido: " + str(sonido))
-        #write_json(dictMedicion)
-        input("Presione cualquier input y enter para obtener otra medicion")
-        #time.sleep(10)
-finally:
-    GPIO.cleanup()
+        
+def callback(canal):
+    if GPIO.input(canal):
+        print(" >> Sonido detectado!")        
+    else:
+        print(" >> Sonido detectado!")
+
+GPIO.add_event_detect(canalSensor, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(canalSensor, callback)
+
+def main():
+    print(" >> Bienvenido! \n")
+    print(" >> A continuacion se activara el sensor por un aproximado de 6 segundos, dentro de este tiempo tienes que \n    generar sonidos (pueden ser aplausos) para encender el numero de leds que deseas. \n")
+    print("        - dos o menos sonidos     : 1 led\n")
+    print("        - entre dos y 4 sonidos   : 2 leds\n")
+    print("        - mas de 4 sonidos        : 3 leds\n\n")
+    input(" >> Presione cualquier tecla y/o enter para iniciar el conteo de input de sonido ")
+    timer = 0
+    ccc=0
+    while timer <= 24:
+        time.sleep(0.25)
+        timer += 1
+        if(GPIO.event_detected(canalSensor)):
+            ccc += 1
+
+    
+    print("El sensor detecto sonido " + str(ccc) + " veces")
+
+    dictMedicion.append(str(ccc))
+
+    write_csv(dictMedicion)
+
+    if ccc == 0:
+        print(" >> No se han detectado sonidos :c")
+    elif ccc <= 2:
+        print(" >> Se ha encendido 1 led!")
+        GPIO.output(canalLed1, GPIO.HIGH)
+    elif ccc == 3 or ccc == 4:
+        print(" >> Se ha encendido 2 leds!")
+        GPIO.output(canalLed1, GPIO.HIGH)
+        GPIO.output(canalLed2, GPIO.HIGH)
+    elif ccc > 4:
+        print(" >> Se ha encendido 3 leds!")
+        GPIO.output(canalLed1, GPIO.HIGH)
+        GPIO.output(canalLed2, GPIO.HIGH)
+        GPIO.output(canalLed3, GPIO.HIGH)
+
+    time.sleep(1)
+    opcion = int(input(" >> Presione 1 para volver a correr el programa\n    Presione 2 para apagar todas las leds\n    Presione 3 para salir"))
+    if opcion == 1:
+        main()
+    elif opcion == 2:
+        GPIO.output(canalLed1, GPIO.LOW)
+        GPIO.output(canalLed2, GPIO.LOW)
+        GPIO.output(canalLed3, GPIO.LOW)
+        main()
+    elif opcion == 3:
+        time.sleep(2)
+        print(" >> Gracias ")
+        GPIO.output(canalLed1, GPIO.LOW)
+        time.sleep(2)
+        print("por utilizar ")
+        GPIO.output(canalLed2, GPIO.LOW)
+        time.sleep(2)
+        print("el programa!")
+        GPIO.output(canalLed3, GPIO.LOW)
+        GPIO.cleanup()
+        print("  >> Creado por:            ")
+        print("       -Andres de la Roca \n")
+        print("       -Marco Jurado      \n")
+        print("       -Jose Auyon      \n\n")
+        print("       Hasta pronto!         ")
+    else:
+        print(" >> ERROR intente nuevamente.")
+            
+
+main()
+
